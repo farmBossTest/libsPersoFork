bf60ecccc3397dcce2bbeb5c5ef37013c530085e
rangiroa <rangiroa26@outlook.fr>
2025-11-23 21:29:30 +0100

    Update geneticAI.h

--- geneticAI/geneticAI.h
+++ geneticAI/geneticAI.h
@@ -1,6 +1,6 @@
 #ifndef GENETICAI
 #define GENETICAI
-//#include "neuralNetwork.h"
+#include "neuralNetwork.h"
 
 
 #define success 1
@@ -21,5 +21,6 @@ int tournament(generation_t gen, float (*match)(neuralNetwork_t, neuralNetwork_t
 int concours (generation_t gen, float (*match)(neuralNetwork_t));                                   // evalue chaque entitée de gen via evaluate, puis reorganise la generation par score croissant. plus la performance du reseau est bonne plus la valeur renvoiée par evaluate doit etre élevée
 
 int averageEntities(entity_t dest, entity_t src1, entity_t src2);                                   // met chaque poids et bias du reseau de neurones de dest a la moyennes du poid ou bias corespondant de src1 et du  poid ou bias corespondant de src2. les reseau de neurone de dest, src1 et src2 doivent avoir la meme taille
+
 
 #endif
